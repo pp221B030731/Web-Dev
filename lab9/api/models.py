af4299fd88f5083e6d7760b1208e37a851dd1fb8
pp221B030731 <n_cherniyazov@kbtu.kz>
2023-04-12 21:00:23 +0600

    lab9

---
+++ lab9/api/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    city = models.CharField(max_length=255)
+    adress = models.TextField()
+
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'city': self.city,
+            'adress': self.adress
+        }
+
+class Vacancy(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    salary = models.FloatField()
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='vacancies')
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'salary': self.salary,
+            'company': self.company.id
+        }
+
